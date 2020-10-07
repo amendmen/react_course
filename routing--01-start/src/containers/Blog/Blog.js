c60fe682430c310e9705e0e686dda213a693c7ea
amendment <alexey.front@bws.pp.ua>
2020-10-07 22:58:34 +0300

    routing wip

---
+++ routing--01-start/src/containers/Blog/Blog.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+import "./Blog.css";
+
+import Posts from "./Posts/Posts";
+
+class Blog extends Component {
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/new-post">New post</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Posts />
+      </div>
+    );
+  }
+}
+
+export default Blog;
