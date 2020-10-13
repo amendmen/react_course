0f227a542afb618d45936a941bece66a833ea50b
amendment <alexey.front@bws.pp.ua>
2020-10-13 22:07:22 +0300

    routing module added

--- routing--01-start/src/containers/Blog/Blog.js
+++ routing--01-start/src/containers/Blog/Blog.js
@@ -3,9 +3,18 @@ import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 import "./Blog.css";
 
 import Posts from "./Posts/Posts";
-import NewPost from "./NewPost/NewPost";
+//import NewPost from "./NewPost/NewPost";
 
+import asyncComponent from "../../hoc/asyncComponent";
+
+const AsyncNewPost = asyncComponent(() => {
+  return import("./NewPost/NewPost");
+});
 class Blog extends Component {
+  state = {
+    auth: true,
+  };
+
   render() {
     return (
       <div className="Blog">
@@ -43,9 +52,12 @@ class Blog extends Component {
         <Route path="/new-post" render={() => <h1>New</h1>} /> */}
 
         <Switch>
-          <Route path="/new-post" component={NewPost} />
+          {this.state.auth ? (
+            <Route path="/new-post" component={AsyncNewPost} />
+          ) : null}
           <Route path="/posts" component={Posts} />
-          <Redirect from="/" to="/posts" />
+          <Route render={() => <h1>Not found</h1>} />
+          {/* <Redirect from="/" to="/posts" /> */}
         </Switch>
       </div>
     );
